506b84e7d40484351b67aadcb1a1782ee1c7d9ab
Colin Gourlay <Gourlay.Colin@abc.net.au>
2018-04-05 16:06:29 +1000

    Wait until CORS headers are sent before continuing.

--- functions/index.js
+++ functions/index.js
@@ -12,64 +12,64 @@ const REFERENCE_ERROR = `Reference does not exist`;
 
 // Return a group, question or answer
 
-exports.get = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {});
+exports.get = functions.https.onRequest((req, res) =>
+  cors(req, res, () => {
+    const { group, question, answer } = req.query;
 
-  const { group, question, answer } = req.query;
+    if (!group || (answer && !question)) {
+      return res.json({ error: QUERY_ERROR });
+    }
 
-  if (!group || (answer && !question)) {
-    return res.json({ error: QUERY_ERROR });
-  }
+    const path = `${group}${question ? `/${question}${answer ? `/${answer}` : ''}` : ''}`;
 
-  const path = `${group}${question ? `/${question}${answer ? `/${answer}` : ''}` : ''}`;
+    db
+      .ref(path)
+      .once('value')
+      .then(snapshot => {
+        const value = snapshot.val();
 
-  db
-    .ref(path)
-    .once('value')
-    .then(snapshot => {
-      const value = snapshot.val();
-
-      res.json(
-        value == null
-          ? { error: REFERENCE_ERROR }
-          : {
-              type: answer ? 'answer' : question ? 'question' : 'group',
-              path,
-              key: answer || question || group,
-              value
-            }
-      );
-    })
-    .catch(error => res.json({ error }));
-});
+        res.json(
+          value == null
+            ? { error: REFERENCE_ERROR }
+            : {
+                type: answer ? 'answer' : question ? 'question' : 'group',
+                path,
+                key: answer || question || group,
+                value
+              }
+        );
+      })
+      .catch(error => res.json({ error }));
+  })
+);
 
 // Increment an answer; return the parent question.
 
-exports.increment = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {});
+exports.increment = functions.https.onRequest((req, res) =>
+  cors(req, res, () => {
+    const { group = UNGROUPED_GROUP, question, answer } = req.query;
 
-  const { group = UNGROUPED_GROUP, question, answer } = req.query;
+    if (!question || !answer) {
+      return res.json({ error: QUERY_ERROR });
+    }
 
-  if (!question || !answer) {
-    return res.json({ error: QUERY_ERROR });
-  }
+    const questionPath = `${group}/${question}`;
+    let questionRef;
 
-  const questionPath = `${group}/${question}`;
-  let questionRef;
-
-  db
-    .ref(group)
-    .transaction(value => (value === null ? {} : undefined))
-    .then(() => (questionRef = db.ref(questionPath)).transaction(value => (value === null ? {} : undefined)))
-    .then(() => db.ref(`${questionPath}/${answer}`).transaction(value => (value === null ? 1 : value + 1)))
-    .then(() => questionRef.once('value'))
-    .then(snapshot =>
-      res.json({
-        type: 'question',
-        path: questionPath,
-        key: question,
-        value: snapshot.val()
-      })
-    )
-    .catch(error => res.json({ error }));
-});
+    db
+      .ref(group)
+      .transaction(value => (value === null ? {} : undefined))
+      .then(() => (questionRef = db.ref(questionPath)).transaction(value => (value === null ? {} : undefined)))
+      .then(() => db.ref(`${questionPath}/${answer}`).transaction(value => (value === null ? 1 : value + 1)))
+      .then(() => questionRef.once('value'))
+      .then(snapshot =>
+        res.json({
+          type: 'question',
+          path: questionPath,
+          key: question,
+          value: snapshot.val()
+        })
+      )
+      .catch(error => res.json({ error }));
+  })
+);
